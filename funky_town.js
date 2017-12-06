67305cb15b9323125509b198615ad07f23bf0cd8
queeniexiang <wxiang1@stuy.edu>
2017-12-06 13:10:34 -0500

    Finished

--- funky_town.js
+++ funky_town.js
@@ -1,3 +1,14 @@
+/* 
+Team HQ
+Helen Ye and Queenie Xiang
+Softdev pd7 
+HW15 -- Sequential Progression
+2017-12-06
+*/
+
+//List of students 
+var students=["Abigail", "Bob", "Claire", "Daniel", "Eve", "Frank"]; 
+
 var fibonacci = function(n) {
     if (n == 1) {
         return 0;
@@ -6,16 +17,29 @@ var fibonacci = function(n) {
         return 1;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
-}
+};
 
+//Euclid's algorithm for GCD 
 var gcd = function(a, b) {
     if (a == b) {
         return a;
     }
-    rem = a % b;
+    var rem = a % b;
     if (rem == 0) {
         return b;
     }
     return gcd(b, rem);
-}
+};
+
+var random_student_selector = function() {
+    var index = Math.random() * students.length 
+    //Floor rounds index down to the previous, lowest integer 
+    index = Math.floor(index);
+    return students[index];
+};
+
+
+
+
+    
 
